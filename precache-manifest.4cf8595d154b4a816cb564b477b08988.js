bd918f101d5dcfac549f31e9d0acd2e7887b567d
Noam Kleiner <nmkleiner@gmail.com>
2019-01-21 21:36:56 +0200

    Updates

---
+++ precache-manifest.4cf8595d154b4a816cb564b477b08988.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/redux-cats/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "8e286c722027e1877195",
+    "url": "/redux-cats/static/css/main.e5f445e0.chunk.css"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/redux-cats/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "c3ea152e295eba65af9c",
+    "url": "/redux-cats/static/js/1.c3ea152e.chunk.js"
+  },
+  {
+    "revision": "f5fb14aec96d69cf5458",
+    "url": "/redux-cats/static/js/runtime~main.f5fb14ae.js"
+  },
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/redux-cats/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/redux-cats/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/redux-cats/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/redux-cats/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/redux-cats/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "8e286c722027e1877195",
+    "url": "/redux-cats/static/js/main.8e286c72.chunk.js"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/redux-cats/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/redux-cats/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/redux-cats/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/redux-cats/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/redux-cats/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/redux-cats/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/redux-cats/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/redux-cats/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "c3ea152e295eba65af9c",
+    "url": "/redux-cats/static/css/1.28193442.chunk.css"
+  },
+  {
+    "revision": "078eba04b336fbc65dcd0abd443a3c8c",
+    "url": "/redux-cats/index.html"
+  }
+];
